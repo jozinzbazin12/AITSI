9b443928454c6f2ffe7e5148b5ca4020552c1e6e
Pawel <p.matwiejuk1993@gmail.com>
2016-04-02 11:07:14 +0200

    linesTable

--- src/PKB/tables/LinesTable.h
+++ src/PKB/tables/LinesTable.h
@@ -8,7 +8,8 @@
 #ifndef SRC_PKB_TABLES_LINESTABLE_H_
 #define SRC_PKB_TABLES_LINESTABLE_H_
 
-#include<vector>
+#include <vector>
+#include <map>
 
 using namespace std;
 
@@ -16,10 +17,16 @@ class LinesTable {
 public:
 	LinesTable();
 	virtual ~LinesTable();
-	vector<int> getIfLines();
-	vector<int> getLoopLines();
+	vector<int> getAssignLines();
+	map<int, vector<int> > getWhileLines(); // w pierwszym parametrze nr tej linijki z napisem while, a w vectorze pozosta³e liniie while
+	// jak bêd¹ potrzebowali stmt to chyba mog¹ sobie zrobic assign i while zamiast pobierac stmt
+	string getLineString(int lineNumber); // zwraca test który byl w linii o tym numerze
 
 private:
+
+	map<int, string> lines; // tutaj po prostu dla kolejnych numerów linii ich tekst (mo¿na na vector zamienic)
+	vector<int> assignLines;
+	map<int, vector<int>> whileLines;
 
 };
 
